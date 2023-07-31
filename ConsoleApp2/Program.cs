66918b71dcbe39fdbd73422b2e96ce733523a7d1
Werang <Pooldeedtinz@gmail.com>
2023-07-31 20:49:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        string myName;
+        myName = "Alex";
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
