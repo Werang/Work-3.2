a9c41476c953a40bb5d2946de8a9b32571f4ae5e
Werang <Pooldeedtinz@gmail.com>
2023-08-07 20:19:25 +0300

    3.7.4 add  FavDay

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,8 +6,11 @@ class MainClass
         Console.Write("Enter your name:");
         string name = Console.ReadLine();
         Console.Write("Enter your age:");
-        byte age = (byte)int.Parse(Console.ReadLine());
+        byte age = checked((byte) int.Parse(Console.ReadLine()));
         Console.WriteLine("Your name is {0} and age is {1}", name, age);
+        Console.Write("What is your favourite day of week?");
+        DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favourite day is {0}", day);
         Console.ReadLine();
     }
 }
