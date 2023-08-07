c52dba8bfab6f0db126a66f6e97ec55510329aa7
Werang <Pooldeedtinz@gmail.com>
2023-08-07 20:04:23 +0300

    2.7.6 INT-BITE

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ class MainClass
         Console.Write("Enter your name:");
         string name = Console.ReadLine();
         Console.Write("Enter your age:");
-        int age = int.Parse(Console.ReadLine());
+        byte age = (byte)int.Parse(Console.ReadLine());
         Console.WriteLine("Your name is {0} and age is {1}", name, age);
         Console.ReadLine();
     }
